5e7e336064abcbf284ea0947e1f241b02924f0e5
Ricardo Araújo <jose.primo.dev@gmail.com>
2019-07-16 23:24:35 -0300

    ultimas atualizacoes 16/07

---
+++ cadastro.js
@@ -0,0 +1,8 @@
+function confirmar(){
+    var senha = document.getElementById("senha").value;
+    var confirme = document.getElementById("confirmsenha").value;
+    if ("senha".value == "confirmsenha".value){
+        window.location.href="cadastro.html";
+        system.out.print("a");
+    }
+}
